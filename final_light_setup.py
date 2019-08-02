4e31a0cc2b60cb2b8ea65d1cd7c387337e8f887d
itskarantyagi <38865668+itskarantyagi@users.noreply.github.com>
2019-08-03 00:41:33 +0530

    Add files via upload

---
+++ final_light_setup.py
@@ -0,0 +1,347 @@
+import sys
+from qtswitch.qt4.QtGui import *
+from qtswitch.qt4.QtCore import *
+
+
+class Final_tool(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setup_ui()
+        self.field = None
+        self.i = None
+        self.f_label = None
+        self.f_field = None
+        self.f_layout = None
+        self.s_label = None
+        self.s_field = None
+        self.s_layout = None
+        self.t_label = None
+        self.t_field = None
+        self.t_layout = None
+        self.radio = None
+        self.light_type = None
+        self.radiobtn = None
+        self.radio = None
+        self.radio_list = []
+        self.btn1 = None
+        self.btn2 = None
+        self.light_type_label = None
+        self.light_type_combo = None
+        self.light_type_hl = None
+        self.radius_field = None
+        self.radius_label = None
+        self.radius_layout = None
+        self.hdr_image_field = None
+        self.hdr_image_label = None
+        self.hdr_layout = None
+        self.push_ibl = None
+        self.selected_btn = None
+        self.optional_widget_label = None
+        self.optional_widget_combo = None
+        self.optional_widget_layout = None
+        self.shadowtype = None
+        self.create_btn = None
+        self.create_btn_hl = None
+        self.hl = None
+        self.shadow_hl1 = None
+        self.shadow_hl = None
+        self.hl1 = None
+        self.shadow_type_filtersize = None
+        self.shadow_type_resolution_field = None
+        self.shadow_type_resolution = None
+        self.shadow_type_filtersize_field = None
+        self.options_for_shadowtype = None
+        self.option_shadowtype_layout = None
+        self.label_shadow_option = None
+
+    def get_characters(self):
+        print ('getting characters in UI')
+        pass
+
+    def get_data(self):
+        pass
+
+    def setup_ui(self):
+        self.main_vl = QVBoxLayout()
+        self.top_hl = QHBoxLayout()
+        self.top1 = QLabel()
+        self.top1.setText("Rigtype")
+        self.top2 = QComboBox()
+        self.top2.addItem("Select")
+        self.top2.addItem("3 Point")
+        self.top2.addItem("Fake GI")
+        self.top2.addItem("IBL")
+        self.top2.currentIndexChanged.connect(self.rigs)
+        self.top_hl.addWidget(self.top1)
+        self.top_hl.addWidget(self.top2)
+        self.main_vl.addLayout(self.top_hl)
+        self.setLayout(self.main_vl)
+
+    def push_btn(self, name):
+        print('push button creation')
+        hl = QHBoxLayout()
+        name_of_btn = QPushButton(name)
+        print(name_of_btn, "HI")
+        hl.addWidget(name_of_btn)
+        self.main_vl.addLayout(hl)
+        name_of_btn.clicked.connect(self.get_data)
+        # print name_of_btn, "within push button method"
+        return name_of_btn, hl
+
+    def Radiobtn(self, radio=[]):
+
+        radio_list = []
+        hl = QHBoxLayout()
+
+        for i in radio:
+            radiobtn = QRadioButton(i)
+            radiobtn.clicked.connect(self.action)
+            radio_list.append(radiobtn)
+            hl.addWidget(radiobtn)
+        self.main_vl.addLayout(hl)
+        radio_list.append(hl)
+        return radio_list
+
+    def action(self):
+        self.choice1 = self.sender().text()
+
+        if self.choice1 == "Character":
+            self.delete_lab_text(
+                objects=[[self.optional_widget_label, self.optional_widget_combo, self.optional_widget_layout]])
+            self.delete_lab_text(objects=[[self.create_btn_hl, self.create_btn]])
+            self.optional_widget_label, self.optional_widget_combo, self.optional_widget_layout = self.lab_combo(
+                lab="List of characters", option=[])
+
+        elif self.choice1 == 'Fixed':
+            self.delete_lab_text(objects=[[self.create_btn_hl, self.create_btn]])
+            self.delete_lab_text(
+                objects=[[self.optional_widget_layout, self.optional_widget_combo, self.optional_widget_label]])
+
+            print "doing for area light"
+        else:
+            pass
+        self.create_btn, self.create_btn_hl = self.push_btn(name="Create")
+        print(self.create_btn, "not inherited_1")
+
+    def lab_combo(self, lab="", option=[]):
+
+        self.hl = QHBoxLayout()
+        self.label = QLabel()
+        self.label.setText(lab)
+
+        self.Combo = QComboBox()
+        for i in option:
+            self.Combo.addItem(i)
+        self.hl.addWidget(self.label)
+        self.hl.addWidget(self.Combo)
+        self.main_vl.addLayout(self.hl)
+        return self.label, self.Combo, self.hl
+
+    def lab_text(self, label="", text=""):
+
+        self.hl = QHBoxLayout()
+        self.label = QLabel()
+        self.label.setText(label)
+        self.field = QLineEdit(text)
+        self.hl.addWidget(self.label)
+        self.hl.addWidget(self.field)
+        self.main_vl.addLayout(self.hl)
+        return self.label, self.field, self.hl
+
+    def shadow_action(self):
+        self.delete_lab_text(
+            objects=[[self.shadow_type_filtersize, self.shadow_type_resolution_field,
+                      self.shadow_type_resolution, self.shadow_type_filtersize_field]])
+        self.delete_lab_text(objects=[[self.create_btn_hl, self.create_btn], [self.shadow_hl, self.shadow_hl1]])
+        self.shadowtype_choice = self.sender().currentText()
+        if self.shadowtype_choice == 'Depth Map':
+            self.shadow_hl = QHBoxLayout()
+            self.shadow_hl1 = QHBoxLayout()
+            self.shadow_type_resolution = QLabel()
+            self.shadow_type_resolution.setText("Resolution")
+            self.shadow_type_resolution_field = QLineEdit("512")
+            self.shadow_hl.addWidget(self.shadow_type_resolution)
+            self.shadow_hl.addWidget(self.shadow_type_resolution_field)
+            self.main_vl.addLayout(self.shadow_hl)
+            self.shadow_type_filtersize = QLabel()
+            self.shadow_type_filtersize.setText("Filter Size")
+            self.shadow_type_filtersize_field = QLineEdit("1")
+            self.shadow_hl1.addWidget(self.shadow_type_filtersize)
+            self.shadow_hl1.addWidget(self.shadow_type_filtersize_field)
+            self.main_vl.addLayout(self.shadow_hl1)
+            self.create_btn, self.create_btn_hl = self.push_btn(name="Create")
+        else:
+            self.create_btn, self.create_btn_hl = self.push_btn(name="Create")
+
+    def fakegi_widget(self):
+        print("Entering fake gi widget")
+        choice = self.light_type_combo.currentText()
+
+        if choice == 'Directional' or choice == 'Area':
+            self.delete_lab_text(objects=[[self.create_btn_hl, self.create_btn]])
+            self.delete_lab_text(
+                objects=[
+                    [self.shadow_type_filtersize, self.shadow_type_resolution_field, self.shadow_hl, self.shadow_hl1,
+                     self.shadow_type_resolution, self.shadow_type_filtersize_field],
+                    [self.create_btn_hl, self.create_btn],
+                    [self.options_for_shadowtype, self.option_shadowtype_layout, self.label_shadow_option]])
+            self.option_shadowtype_layout = QHBoxLayout()
+            self.label_shadow_option = QLabel()
+            self.label_shadow_option.setText("Shadow Type")
+            self.options_for_shadowtype = QComboBox()
+
+            self.options_for_shadowtype.addItem("Ray Trace")
+            self.option_shadowtype_layout.addWidget(self.label_shadow_option)
+
+            self.option_shadowtype_layout.addWidget(self.options_for_shadowtype)
+            self.main_vl.addLayout(self.option_shadowtype_layout)
+            self.create_btn, self.create_btn_hl = self.push_btn(name="Create")
+
+        elif choice == 'Spot':
+            self.delete_lab_text(objects=[[self.create_btn_hl, self.create_btn]])
+            self.delete_lab_text(
+                objects=[
+                    [self.shadow_type_filtersize, self.shadow_type_resolution_field, self.shadow_hl, self.shadow_hl1,
+                     self.shadow_type_resolution, self.shadow_type_filtersize_field],
+                    [self.create_btn_hl, self.create_btn],
+                    [self.options_for_shadowtype, self.option_shadowtype_layout, self.label_shadow_option]])
+            self.option_shadowtype_layout = QHBoxLayout()
+            self.label_shadow_option = QLabel()
+            self.label_shadow_option.setText("Shadow Type")
+            self.options_for_shadowtype = QComboBox()
+            self.options_for_shadowtype.addItem("Select")
+            self.options_for_shadowtype.addItem("Depth Map")
+            self.options_for_shadowtype.addItem("Ray Trace")
+            self.option_shadowtype_layout.addWidget(self.label_shadow_option)
+            self.option_shadowtype_layout.addWidget(self.options_for_shadowtype)
+            self.main_vl.addLayout(self.option_shadowtype_layout)
+            self.options_for_shadowtype.currentIndexChanged.connect(self.shadow_action)
+
+
+
+        else:
+            self.delete_lab_text(objects=[[self.create_btn_hl, self.create_btn]])
+            # self.delete_lab_text(objects=[[self.shadowtype]])
+            pass
+
+        # print(self.create_btn, "not inherited_2")
+
+    def rigs(self):
+        # print "its working fine right now"
+        choice = self.top2.currentText()
+        if choice == '3 Point':
+            # self.delete_lab_text(objects=[[self.light_type_label, self.light_type_combo, self.light_type_hl]])
+            self.delete_lab_text(
+                objects=[[self.f_label, self.f_field, self.f_layout], [self.s_label, self.s_field, self.s_layout],
+                         [self.t_label, self.t_field, self.t_layout], [self.btn1],
+                         [self.light_type_label, self.light_type_combo, self.light_type_hl],
+                         [self.radius_label, self.radius_field, self.radius_layout],
+                         [self.hdr_image_label, self.hdr_image_field, self.hdr_layout], [self.selected_btn],
+                         [self.push_ibl],
+                         [self.optional_widget_label, self.optional_widget_layout, self.optional_widget_combo],
+                         [self.create_btn, self.create_btn_hl], [self.shadowtype],
+                         [self.hl1, self.shadow_type_filtersize, self.shadow_type_resolution_field, self.hl,
+                          self.shadow_type_resolution, self.shadow_type_filtersize_field],
+                         [self.option_shadowtype_layout, self.options_for_shadowtype, self.label_shadow_option]])
+            self.f_label, self.f_field, self.f_layout = self.lab_text('Key', '1.0')
+            self.s_label, self.s_field, self.s_layout = self.lab_text('Rim', '1.0')
+            self.t_label, self.t_field, self.t_layout = self.lab_text('Fill', '1.0')
+            self.btn1 = self.Radiobtn(radio=["Fixed", "Character"])
+            self.btn1[1].clicked.connect(self.get_characters)
+
+            print(self.btn1)
+
+            print("Getting out of 3 Point")
+
+        if choice == 'Fake GI':
+            self.delete_lab_text(objects=[self.btn1])
+            # self.delete_lab_text([[self.radius_label, self.radius_field, self.radius_layout],
+            #                       [self.hdr_image_label, self.hdr_image_field, self.hdr_layout],
+            #                       [self.create_btn, self.create_btn_hl]])
+            self.delete_lab_text(
+                objects=[[self.f_label, self.f_field, self.f_layout], [self.s_label, self.s_field, self.s_layout],
+                         [self.t_label, self.t_field, self.t_layout], [self.btn1],
+                         [self.light_type_label, self.light_type_combo, self.light_type_hl],
+                         [self.radius_label, self.radius_field, self.radius_layout],
+                         [self.hdr_image_label, self.hdr_image_field, self.hdr_layout], [self.selected_btn],
+                         [self.push_ibl],
+                         [self.optional_widget_label, self.optional_widget_layout, self.optional_widget_combo],
+                         [self.create_btn, self.create_btn_hl], [self.shadowtype],
+                         [self.hl1, self.shadow_type_filtersize, self.shadow_type_resolution_field, self.hl,
+                          self.shadow_type_resolution, self.shadow_type_filtersize_field],
+                         [self.option_shadowtype_layout, self.options_for_shadowtype, self.label_shadow_option]])
+            self.f_label, self.f_field, self.f_layout = self.lab_combo(lab="Number of Lights",
+                                                                       option=['Select', '14', '22', '32', '44', '58',
+                                                                               '74', '92', '112', '134', '158', '184',
+                                                                               '212', '242', '274', '308', '344',
+                                                                               '382'])
+            self.s_label, self.s_field, self.s_layout = self.lab_text('Radius', '10.0')
+            self.t_label, self.t_field, self.t_layout = self.lab_text('Intensity', '1.0')
+            self.light_type_label, self.light_type_combo, self.light_type_hl = self.lab_combo(
+                lab='Light Type (Default : Directional)',
+                option=['Select',
+                        'Directional',
+                        'Spot', 'Area'])
+            self.light_type_combo.currentIndexChanged.connect(self.fakegi_widget)
+            print("Getting out of fake Gi")
+
+        if choice == 'IBL':
+            self.delete_lab_text(objects=[self.btn1])
+            # self.delete_lab_text(objects=[[self.light_type_label, self.light_type_combo, self.light_type_hl]])
+            self.delete_lab_text(
+                objects=[[self.f_label, self.f_field, self.f_layout], [self.s_label, self.s_field, self.s_layout],
+                         [self.t_label, self.t_field, self.t_layout], [self.btn1],
+                         [self.light_type_label, self.light_type_combo, self.light_type_hl],
+                         [self.radius_label, self.radius_field, self.radius_layout],
+                         [self.hdr_image_label, self.hdr_image_field, self.hdr_layout], [self.selected_btn],
+                         [self.push_ibl],
+                         [self.optional_widget_label, self.optional_widget_layout, self.optional_widget_combo],
+                         [self.create_btn, self.create_btn_hl], [self.shadowtype],
+                         [self.hl1, self.shadow_type_filtersize, self.shadow_type_resolution_field, self.hl,
+                          self.shadow_type_resolution, self.shadow_type_filtersize_field],
+                         [self.option_shadowtype_layout, self.options_for_shadowtype, self.label_shadow_option]])
+
+            self.radius_label, self.radius_field, self.radius_layout = self.lab_text('Radius', '1.0')
+            self.hdr_image_label, self.hdr_image_field, self.hdr_layout = self.lab_text('HDR Image', '/u/ktya/path')
+            # print("Create btn not getting created")
+            self.create_btn, self.create_btn_hl = self.push_btn(name="Create")
+            # print("Create btn created")
+            print("Getting out of IBL")
+
+        if choice == 'Select':
+            self.delete_lab_text(
+                objects=[[self.f_label, self.f_field, self.f_layout], [self.s_label, self.s_field, self.s_layout],
+                         [self.t_label, self.t_field, self.t_layout], self.btn1,
+                         [self.light_type_label, self.light_type_combo, self.light_type_hl],
+                         [self.radius_label, self.radius_field, self.radius_layout],
+                         [self.hdr_image_label, self.hdr_image_field, self.hdr_layout], [self.selected_btn],
+                         [self.push_ibl],
+                         [self.optional_widget_label, self.optional_widget_layout, self.optional_widget_combo],
+                         [self.create_btn, self.create_btn_hl], [self.shadowtype],
+                         [self.hl1, self.shadow_type_filtersize, self.shadow_type_resolution_field, self.hl,
+                          self.shadow_type_resolution, self.shadow_type_filtersize_field],
+                         [self.option_shadowtype_layout, self.options_for_shadowtype, self.label_shadow_option]])
+
+    def delete_lab_text(self, objects=[]):
+        for obj in objects:
+            if not obj:
+                return
+            for ob in obj:
+                try:
+                    ob.setParent(None)
+                    ob.deleteLater()
+                except:
+                    pass
+
+
+if __name__ == "__main__":
+    # print "this is final_light_setup main file"
+    app = QApplication([])
+    mw = QMainWindow()
+    wdg = Final_tool()
+    wdg.show()
+    sys.exit(app.exec_())
+else:
+    print "its inherited in light_tool_maya_command"
+
+
---
+++ light_tool_maya_commands.py
@@ -0,0 +1,263 @@
+import maya.cmds as cmds
+import sys
+import final_light_setup
+
+reload(final_light_setup)
+from final_light_setup import Final_tool
+from qtswitch.qt4.QtGui import *
+
+
+class Light_tool_maya_commands(Final_tool):
+
+    def __init__(self, *args):
+        Final_tool.__init__(self, *args)
+        # print self.create_btn, "the inherited button"
+        self.get_data = self.fetch
+        # self.show()
+        self.get_characters = self.update_characters
+        # self.shadow_action = self.user_shadow
+
+    # def user_shadow(self):
+
+    def update_characters(self):
+
+        if (self.choice1 == 'Character'):
+            # shapes = cmds.ls(type='mesh')
+            # transform_node = cmds.listRelatives(shapes, parent=True)
+            # for i in transform_node:
+            #     print(">>>")
+            self.optional_widget_combo.addItems(cmds.ls("*global_ctrl"))
+
+    def fetch(self):
+        print("entering fetch")
+        rig_choice = self.top2.currentText()
+        # if rig_choice == '3 Point':
+
+        if rig_choice == "3 Point":
+            print("Entering 3 Point")
+            self.user_dict = {rig_choice: {
+                self.f_label.text(): self.f_field.text(),
+                self.s_label.text(): self.s_field.text(),
+                self.t_label.text(): self.t_field.text(),
+                "Fixed/Character": self.choice1
+            }}
+
+            if (self.choice1 == 'Character'):
+                self.light_values = self.Combo.currentIndexChanged.connect(self.character_light_setup)
+
+            key = cmds.spotLight(i=float(self.f_field.text()), name='key_light')
+            fill = cmds.spotLight(i=float(self.s_field.text()), name='fill_light')
+            rim = cmds.spotLight(i=float(self.t_field.text()), name='rim_light')
+            cmds.setAttr("{0}.useRayTraceShadows".format(fill), 0)
+            cmds.setAttr("{0}.useRayTraceShadows".format(rim), 0)
+            key_tr = cmds.listRelatives(key, p=True)[0]
+            fill_tr = cmds.listRelatives(fill, p=True)[0]
+            rim_tr = cmds.listRelatives(rim, p=True)[0]
+
+            key_loc = cmds.spaceLocator(n="{0}_aim".format(key_tr))[0]
+            fill_loc = cmds.spaceLocator(n="{0}_aim".format(fill_tr))[0]
+            rim_loc = cmds.spaceLocator(n="{0}_aim".format(rim_tr))[0]
+
+            key_grp = cmds.group(key_tr, name="key_lit_grp")
+            fill_grp = cmds.group(fill_tr, name="fill_lit_grp")
+            rim_grp = cmds.group(rim_tr, name="rim_lit_grp")
+
+            cmds.xform(key_tr, ws=True, t=(10, 10, 7))
+            cmds.xform(fill_tr, ws=True, t=(-10, -6, 5))
+            cmds.xform(rim_tr, ws=True, t=(10, 10, -7))
+
+            cmds.aimConstraint(key_loc, key_tr, mo=False,
+                               aim=(0, 0, -1))
+            cmds.aimConstraint(fill_loc, fill_tr, mo=False,
+                               aim=(0, 0, -1))
+            cmds.aimConstraint(rim_loc, rim_tr, mo=False,
+                               aim=(0, 0, -1))
+
+            cmds.group([key_loc, fill_loc, rim_loc, key_grp, fill_grp, rim_grp], name='three_point_light_rig')
+
+        elif rig_choice == "Fake GI":
+            print("Entering Fake GI")
+            self.user_dict = {rig_choice: {
+                self.f_label.text(): self.f_field.currentText(),
+                self.s_label.text(): self.s_field.text(),
+                self.t_label.text(): self.t_field.text(),
+                "Light Type": self.light_type_combo.currentText(),
+                "Shadow type": self.options_for_shadowtype.currentText()
+            }}
+
+
+            radius = float(self.s_field.text())
+            intensity = float(self.t_field.text())
+            count_data = {}
+
+            for c, i in enumerate(range(3, 21)):
+                print i * (i - 1) + 2
+                count_data[i * (i - 1) + 2] = i
+
+            try:
+                light_count = int(self.f_field.currentText())
+            except:
+                msg = QDialog()
+                msg.setFixedSize(300, 100)
+                vl = QVBoxLayout()
+                msg_txt = QLabel("Please Select Number of Lights.")
+                vl.addWidget(msg_txt)
+                ok_btn = QPushButton("Ok")
+                ok_btn.clicked.connect(msg.close)
+                vl.addWidget(ok_btn)
+                msg.setLayout(vl)
+                msg.setModal(True)
+                msg.show()
+
+            cmds.polySphere(r=radius, sx=count_data[light_count], sy=count_data[light_count])
+
+            obj = cmds.ls(sl=True)
+            vtxes = cmds.ls("{0}.vtx[*]".format(obj[0]), fl=True)
+            lights = []
+            for vtx in vtxes:
+                pos = cmds.xform(vtx, ws=True, q=True, t=True)
+
+                choice = self.light_type_combo.currentText()
+
+                if choice == 'Directional':
+                    sl = cmds.directionalLight(i=intensity)
+                elif choice == 'Spot':
+                    sl = cmds.spotLight(i=intensity)
+                    # resolution = int(self.shadow_type_resolution_field.text())
+
+                    # filter_size = float(self.shadow_type_filtersize_field.text())
+
+                    if self.shadowtype_choice == "Depth Map":
+                        cmds.setAttr("{0}.useDepthMapShadows".format(sl), 1)
+
+                        try:
+                            resolution = int(self.shadow_type_resolution_field.text())
+                        except:
+                            msg = QDialog()
+                            msg.setFixedSize(350, 100)
+                            vl = QVBoxLayout()
+                            msg_txt = QLabel("Please enter Resolution/ Datatype entered is not supported")
+                            vl.addWidget(msg_txt)
+                            ok_btn = QPushButton("Ok")
+                            ok_btn.clicked.connect(msg.close)
+                            vl.addWidget(ok_btn)
+                            msg.setLayout(vl)
+                            msg.setModal(True)
+                            msg.show()
+
+                        cmds.setAttr("{0}.dmapResolution".format(sl), resolution)
+
+                        try:
+                            filter_size = float(self.shadow_type_filtersize_field.text())
+                        except:
+                            msg = QDialog()
+                            msg.setFixedSize(350, 100)
+                            vl = QVBoxLayout()
+                            msg_txt = QLabel("Please enter Filter size/ Datatype entered is not supported")
+                            vl.addWidget(msg_txt)
+                            ok_btn = QPushButton("Ok")
+                            ok_btn.clicked.connect(msg.close)
+                            vl.addWidget(ok_btn)
+                            msg.setLayout(vl)
+                            msg.setModal(True)
+                            msg.show()
+
+                        cmds.setAttr("{0}.dmapFilterSize".format(sl), filter_size)
+
+                elif choice == 'Area':
+                    sl = cmds.shadingNode('areaLight', asLight=True)
+                else:
+                    sl = cmds.directionalLight(i=intensity)
+                if choice == 'Area':
+                    sl_tr = sl
+                else:
+                    sl_tr = cmds.listRelatives(sl, p=True)[0]
+                cmds.xform(sl_tr, ws=True, t=pos)
+                loc = cmds.spaceLocator()
+                con = cmds.aimConstraint(loc[0], sl_tr, aim=(0, 0, -1))
+                cmds.delete(con, loc)
+                lights.append(sl_tr)
+            cmds.delete(obj)
+            cmds.group(lights, name="GeoJo")
+
+        elif rig_choice == "IBL":
+
+            self.user_dict = {rig_choice: {
+                self.radius_label.text(): self.radius_field.text(),
+                self.hdr_image_label.text(): self.hdr_image_field.text()
+            }}
+
+            radius = float(self.radius_field.text())
+
+            path = str(self.hdr_image_field.text())
+            res = cmds.polySphere(r=radius)
+            lam = cmds.shadingNode('lambert', asShader=True)
+            cmds.select(res[0])
+            cmds.hyperShade(assign=lam)
+            cmds.setAttr("{0}.diffuse".format(lam), 0)
+            fl = cmds.shadingNode('file', asTexture=True)
+            cmds.connectAttr("{0}.outColor".format(fl), "{0}.incandescence".format(lam))
+            cmds.setAttr("{0}.ftn".format(fl), path, type='string')
+
+        else:
+            pass
+        self.create_point_lights()
+        return self.user_dict
+
+    def create_point_lights(self):
+        for k, v in self.user_dict.items():
+            print(k, v)
+            for ik, iv in v.items():
+                print (ik, iv)
+
+    def character_light_setup(self):
+        choice = self.Combo.currentText()
+        cmds.select(choice)
+        translation = cmds.getAttr({0}.format(choice))
+        return translation
+
+    def error_correction(self, type="", text_field_name="", message=""):
+
+        if type == "int":
+
+            try:
+                variable = float("{0}.currentText()".format(text_field_name))
+            except:
+                msg = QDialog(self)
+                msg.setFixedSize(300, 100)
+                vl = QVBoxLayout()
+                msg_txt = QLabel(message)
+                vl.addWidget(msg_txt)
+                ok_btn = QPushButton("Ok")
+                ok_btn.clicked.connect(msg.close)
+                vl.addWidget(ok_btn)
+                msg.setLayout(vl)
+                msg.setModal(True)
+                msg.show()
+
+        if type == "float":
+            try:
+                variable = float("{0}.currentText()".format(text_field_name))
+            except:
+                msg = QDialog(self)
+                msg.setFixedSize(300, 100)
+                vl = QVBoxLayout()
+                msg_txt = QLabel(message)
+                vl.addWidget(msg_txt)
+                ok_btn = QPushButton("Ok")
+                ok_btn.clicked.connect(msg.close)
+                vl.addWidget(ok_btn)
+                msg.setLayout(vl)
+                msg.setModal(True)
+                msg.show()
+            return variable
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    mw = QMainWindow()
+    wdg = Light_tool_maya_commands()
+    wdg.show()
+    sys.exit(app.exec_())
+
+
